f286a33b34d82374145474443ee5bbefa68f4994
Davit Gabashvili <datigabashvili@gmail.com>
2026-05-06 14:08:50 +0400

    add https on in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,8 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (config('app.url') === 'https://api.arttteo.com') {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
+
+        request()->server->set('HTTPS', 'on');
     }
 }
